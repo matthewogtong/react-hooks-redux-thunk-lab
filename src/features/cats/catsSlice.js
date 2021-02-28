87beb0ff3f21be939593b83c5a49f32cc68cfc2f
matthewogtong <ogtongm@gmail.com>
2021-02-28 16:42:02 -0500

    commit tree dd4520550a2da4794abdce16b95f1d29502eccfa

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,37 @@
-export function fetchCats() {}
+// Action Creators
+export function fetchCats() {
+    return function(dispatch) {
+        dispatch({ type: "cats/catsLoading" })
+        fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+            .then(r => r.json())
+            .then(data => {
+                dispatch({ type: "cats/catsLoaded", payload: data.images })
+            })
+    }
+}
 
-export default function catsReducer() {}
+
+
+// Reducers
+const initialState = {
+    entities: [],
+    status: "idle"
+}
+export default function catsReducer(state = initialState, action) {
+    const { type, payload } = action
+    switch (type) {
+        case "cats/catsLoading":
+            return {
+                ...state,
+                status: "loading"
+            }
+        case "cats/catsLoaded":
+            return {
+                ...state,
+                entities: payload,
+                status: "idle"
+            }
+        default:
+            return state
+    }
+}
