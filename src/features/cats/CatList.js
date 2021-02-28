b8294602213e40d0578fa5888709156d846f0eb6
matthewogtong <ogtongm@gmail.com>
2021-02-28 16:42:05 -0500

    Done.

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,21 @@
-// write your CatList component here
+import React from "react"
+import { useSelector } from "react-redux"
+
+const CatList = ({ catPics = [] }) => {
+
+    const isLoading = useSelector(state => state.status === "loading")
+
+    const displayCatPics = catPics.map(pic => {
+        return (
+            <img key={pic.id} src={pic.url} alt="cat" />
+        )
+    })
+
+    return (
+        <div>
+            {isLoading ? <p>Loading...</p> : displayCatPics}
+        </div>
+    )
+}
+
+export default CatList
